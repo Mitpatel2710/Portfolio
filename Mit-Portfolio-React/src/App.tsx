3450ffbb8048c6a453ce0760033590cc367f9c83
mitpatel <mitpatel2710@gmail.com>
2024-12-24 17:06:57 -0600

    name change

--- Mit-Portfolio-React/src/App.tsx
+++ Mit-Portfolio-React/src/App.tsx
@@ -44,7 +44,7 @@ export default function App() {
       
       <footer className="bg-gray-900 text-white py-8">
         <div className="container mx-auto px-6 text-center">
-          <p>© 2024 John Doe. All rights reserved.</p>
+          <p>© 2023 Mit Patel. All rights reserved.</p>
         </div>
       </footer>
     </div>
